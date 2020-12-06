487941f6530a3802cbc806f48852140716d683c3
Mahesh552001 <68393025+Mahesh552001@users.noreply.github.com>
2020-12-06 13:11:53 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const mah = document.querySelector(".ram")
+const name = document.querySelector("#name")
+ console.log('came in')
+//  mah.onsubmit
+function power(){
+    window.alert("Dear "+name.value+" ,Your appoinment has been booked successfully, Thank You!!")
+}
+
