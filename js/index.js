91a60f4f2947f4018168d8522b90bee7d6799cd2
Mahesh552001 <68393025+Mahesh552001@users.noreply.github.com>
2021-01-19 13:21:47 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const message = document.querySelector("#message")
 //  mah.onsubmit
 function power(){
     var x = names.push(name.value)
-    window.alert("Dear "+name.value+" ,Your appoinment with token number " + names.push()+" has been booked successfully, Thank You!!")
+    window.alert("Dear "+name.value+" ,Your appoinment with token number " + names.push()+" has been booked successfully, Refer your mail for timings, Thank You!!")
     name.value = '';
     email.value = '';
     date.value = '';
