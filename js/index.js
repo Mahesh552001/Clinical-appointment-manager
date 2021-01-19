2b37ac31ee6b8c11f8a5ab4bc6694645574f8278
Mahesh552001 <68393025+Mahesh552001@users.noreply.github.com>
2021-01-19 09:56:24 +0530

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const mah = document.querySelector(".ram")
-const name = document.querySelector("#name")
- console.log('came in')
-//  mah.onsubmit
-function power(){
-    window.alert("Dear "+name.value+" ,Your appoinment has been booked successfully, Thank You!!")
-}
-
