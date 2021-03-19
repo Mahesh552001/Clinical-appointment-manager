a725197995ad1d84b4b366eca3d4d34c80da1946
Mahesh552001 <68393025+Mahesh552001@users.noreply.github.com>
2021-03-19 10:11:58 +0530

    Add files via upload

---
+++ booking.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html> 
+<html> 
+  
+<head> 
+    <title>Confirmation page</title> 
+</head> 
+  
+<body> 
+    <center> 
+        <?php 
+  
+        // servername => localhost 
+        // username => root 
+        // password => empty 
+        // database name => Mahesh 
+        $conn = mysqli_connect("sql312.epizy.com", "epiz_28181131", "W8aFz1KHravgd", "epiz_28181131_webcam"); 
+          
+        // Check connection 
+        if($conn === false){ 
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error()); 
+        } 
+          
+        $name =  $_REQUEST['name'];
+		$email = $_REQUEST['email'];
+		$date = $_REQUEST['date'];
+		$phone = $_REQUEST['phone'];
+		$message = $_REQUEST['message'];
+          
+      
+        $sql = "INSERT INTO hospital  VALUES (NULL,'$name',  
+            '$email','$date','$phone','$message')"; 
+          
+        if(mysqli_query($conn, $sql)){ 
+            echo "<h3>Your appointment has been booked successfully.\n" 
+                . " For further clarification please refer your email.\n" 
+                . " Thank you</h3>";  
+  
+            echo nl2br("\n$name\n $email\n "
+                . "$date\n $phone\n"); 
+        } else{ 
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn); 
+        }
+        session_start();
+        $_SESSION['Name'] = $name;
+        $_SESSION['Email'] = $email;
+
+        echo "<h3>To view current appointments-<a href='fetch.php'>click here</a></h3>";
+          
+        // Close connection 
+        mysqli_close($conn); 
+        ?> 
+    </center> 
+</body> 
+  
+</html> 
---
+++ fetch.php
@@ -0,0 +1,102 @@
+
+<?php 
+  
+
+$user = 'epiz_28181131'; 
+$password = 'W8aFz1KHravgd';  
+  
+
+$database = 'epiz_28181131_webcam';  
+  
+
+$servername='sql312.epizy.com'; 
+$mysqli = new mysqli($servername, $user,  
+                $password, $database); 
+
+if ($mysqli->connect_error) { 
+    die('Connect Error (' .  
+    $mysqli->connect_errno . ') '.  
+    $mysqli->connect_error); 
+} 
+
+session_start();
+$name = $_SESSION['Name'];
+$email = $_SESSION['Email'];
+  
+
+$sql = "SELECT * FROM hospital WHERE name='$name' AND email='$email'"; 
+$result = $mysqli->query($sql); 
+$mysqli->close();  
+?> 
+<!DOCTYPE html> 
+<html lang="en"> 
+  
+<head> 
+    <meta charset="UTF-8"> 
+    <title>User Details</title> 
+   
+    <style> 
+        table { 
+            margin: 0 auto; 
+            font-size: large; 
+            border: 1px solid black; 
+        } 
+  
+        h1 { 
+            text-align: center; 
+            color: #006600; 
+            font-size: xx-large; 
+            font-family: 'Gill Sans', 'Gill Sans MT',  
+            ' Calibri', 'Trebuchet MS', 'sans-serif'; 
+        } 
+  
+        td { 
+            background-color: #E4F5D4; 
+            border: 1px solid black; 
+        } 
+  
+        th, 
+        td { 
+            font-weight: bold; 
+            border: 1px solid black; 
+            padding: 10px; 
+            text-align: center; 
+        } 
+  
+        td { 
+            font-weight: lighter; 
+        } 
+    </style> 
+</head> 
+  
+<body> 
+    <section> 
+        <h1>Appointments</h1> 
+        
+        <table> 
+            <tr>
+			    <th>No.</th>
+                <th>Name</th> 
+                <th>Email</th> 
+                <th>Date</th> 
+                <th>Phone</th>
+				<th>Message</th>
+
+            </tr> 
+           
+            <?php     
+                $rows = mysqli_fetch_row($result); 
+             ?> 
+            <tr> 
+                <td><?php echo $rows[0];?></td>
+                <td><?php echo $rows[1];?></td> 
+                <td><?php echo $rows[2];?></td> 
+                <td><?php echo $rows[3];?></td> 
+                <td><?php echo $rows[4];?></td> 
+				<td><?php echo $rows[5];?></td>
+            </tr> 
+        </table> 
+    </section> 
+</body> 
+  
+</html> 
